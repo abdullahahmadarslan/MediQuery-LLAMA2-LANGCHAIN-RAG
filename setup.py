5d1d13f501e66e5aa18d8d61c76e0b738449b5b1
Abdullah Ahmad Arslan <abdullah.ahmad.arslan@gmail.com>
2025-01-25 15:20:33 +0500

    template added + setup of local package done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediQuery',
+    version= '0.0.0',
+    author= 'Abdullah Ahmad Arslan',
+    author_email= 'abdullah.ahmad.arslan125@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
